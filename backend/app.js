f9a1cb7426192e49ae2f7f5c189d9ff00fe162ab
sam <sfriedman6711@gmail.com>
2024-01-24 12:23:38 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -11,29 +11,32 @@ const isProduction = environment === "production";
 
 const app = express();
 
-// middleware for logging information about requests and responses
+// Beginning of Middleware:
+
+// Middleware for logging information about requests and responses.
 app.use(morgan("dev"));
 
-//  middleware for parsing cookies
+//  Middleware for parsing cookies.
 app.use(cookieParser());
 
-//  middleware for parsing JSON bodies of requests with Content-Type of "application/json".
+//  Middleware for parsing JSON bodies of requests with Content-Type of "application/json".
 app.use(express.json());
 
-// Security Middleware
+// Begin Security Middleware
+
 if (!isProduction) {
-  // enable cors only in development
+  // Enable cors only in development.
   app.use(cors());
 }
 
-// helmet helps set a variety of headers to better secure app
+// Helmet helps set a variety of headers to better secure app.
 app.use(
   helmet.crossOriginResourcePolicy({
     policy: "cross-origin",
   })
 );
 
-// Set the _csrf token and create req.csrfToken method
+// Set the _csrf token and create req.csrfToken method.
 app.use(
   csurf({
     cookie: {
@@ -44,8 +47,53 @@ app.use(
   })
 );
 
+// End Security Middleware
+
+// Begin Routes/Errorhandlers Middleware
+
 const routes = require("./routes");
-// Connect all the routes
+// Connect all the routes.
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+const { ValidationError } = require('sequelize');
+
+// Process sequelize errors.
+app.use((err, _req, _res, next) => {
+  // Check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+// End Routes/Errorhandlers Middleware
+
+//End of Middleware
 
 module.exports = app;
