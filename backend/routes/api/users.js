d1b4f2a376930a6812aca06a74977a6569cbb3e7
sam <sfriedman6711@gmail.com>
2024-01-25 16:12:29 -0700

    Add user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
+//Handles validation errors that occur with the body of a request, attached to Sign up route
+const validateSignup = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
+];
+
 //Sign up route -- takes in username, email, password, hashes with bcrypt, awaits successful create, uses setTokenCookie() to create JWT cookie with the non-sensitive information as the payload
-router.post("/", async (req, res) => {
+router.post("/", validateSignup, async (req, res) => {
   const { email, password, username } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({ email, username, hashedPassword });
