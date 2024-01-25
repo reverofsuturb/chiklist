9475733b43e5894e157d4e10d76b1cc6ac740958
sam <sfriedman6711@gmail.com>
2024-01-25 16:07:22 -0700

    Validation middleware attached to login route, tested

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,13 +2,28 @@ const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
+//Handles validation errors that occur with the body of a request, attached to Log In Route
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
 //Log In Route
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
   //unscoped removes the default scope added to User
   const user = await User.unscoped().findOne({
@@ -55,7 +70,6 @@ router.get("/", (req, res) => {
     });
   } else return res.json({ user: null });
 });
-
 
 //Log Out route; removes the token cookie and returns a JSON success message
 // _req denotes not being used
