0629d3f31caab2609968f9564af3b3b5a0904f19
sam <sfriedman6711@gmail.com>
2024-01-25 17:03:05 -0700

    seed update to reflect user model update

--- backend/db/seeders/20240125205525-demo-user.js
+++ backend/db/seeders/20240125205525-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Brad',
+        lastName: 'Jenkins',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Larry',
+        lastName: 'Bird',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Guacialata',
+        lastName: 'Smith',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
